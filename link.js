015e97f42cd2eb201499cc239b9553f91cf1397f
Aayush Khanal <83815606+khanalashim@users.noreply.github.com>
2023-03-05 22:48:12 +0545

    Delete link.js

--- link.js
+++
@@ -1,9 +0,0 @@
-
-        function fun(){
-            const txt = document.getElementById('hero-field').value.length;
-            if (txt<20){
-              alert('Invalid TRX address..');
-            } else {
-              location.assign('dashboard.html');
-            }
-        }
